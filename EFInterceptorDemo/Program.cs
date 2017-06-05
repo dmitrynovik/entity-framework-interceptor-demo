96e145c06274eb52ba4e07ee6babbb6a8788d556
Dmitry Novik <dmitry.novik@vivant.com.au>
2017-06-05 15:56:17 +1000

    update with different context case added to demo

--- EFInterceptorDemo/Program.cs
+++ EFInterceptorDemo/Program.cs
@@ -10,24 +10,25 @@ namespace EFInterceptorDemo
         {
             var name = Guid.NewGuid().ToString();
 
-            using (var ctx = new DataContext(2, 2))
-            {
-                var stopwatch = Stopwatch.StartNew();
-                const int tries = 1000;
-                for (int i = 0; i < tries; i++)
-                {
-                    var obj = ctx.Employees.FirstOrDefault(x => x.Id == i);
-                }
-                stopwatch.Stop();
-                Console.Write("Elapsed: {0}", stopwatch.Elapsed);
-            }
-            Console.Read();
+            //using (var ctx = new DataContext(2, 2))
+            //{
+            //    var stopwatch = Stopwatch.StartNew();
+            //    const int tries = 1000;
+            //    for (int i = 0; i < tries; i++)
+            //    {
+            //        var obj = ctx.Employees.FirstOrDefault(x => x.Id == i);
+            //    }
+            //    stopwatch.Stop();
+            //    Console.Write("Elapsed: {0}", stopwatch.Elapsed);
+            //}
+            //Console.Read();
 
+            Employee employee = null;
             using (var ctx = new DataContext(tenantId: 2, userId: 66))
             {
                 Console.WriteLine("adding new employee with name {0} to tenant {1}", name, ctx.TenantId);
                 // INSERT (will assign automatically TenantId, CreatedBy, CreatedOn, ModifiedBy, ModifiedOn):
-                var employee = new Employee {Name = name, Salary = 100};
+                employee = new Employee {Name = name, Salary = 100};
                 ctx.Employees.Add(employee);
                 Debug.Assert(ctx.SaveChanges() == 1);
             }
@@ -35,17 +36,27 @@ namespace EFInterceptorDemo
             using (var ctx = new DataContext(tenantId: 2, userId: 77))
             {
                 // UPDATE: (will verify TenantId, modify ModifiedBy, ModifiedOn):
-                var employee = ctx.Employees.First(x => x.Name == name);
+                employee = ctx.Employees.First(x => x.Name == name);
                 var salary = employee.Salary + 1;
-                Console.WriteLine("\nupdating employee {0} with salary {1}", name, salary);
                 employee.Salary = salary;
-                Debug.Assert(ctx.SaveChanges() == 1);
+                var recNum = ctx.SaveChanges();
+                Console.WriteLine("\nupdating employee {0} with salary {1} updated {2} records", name, salary, recNum);
+                Debug.Assert(recNum == 1);
+            }
+
+            using (var ctx = new DataContext(tenantId: 3, userId: 77))
+            {
+                var salary = employee.Salary + 1;
+                employee.Salary = salary;
+                var recNum = ctx.SaveChanges();
+                Console.WriteLine("\nupdating employee {0} with salary {1} updated {2} records", name, salary, recNum);
+                Debug.Assert(recNum == 0);
             }
 
             using (var ctx = new DataContext(tenantId: 2, userId: 66))
             {
                 // SELECT (will automatically add TenantId = 2 clause):
-                var employee = ctx.Employees.First(x => x.Name == name);
+                employee = ctx.Employees.First(x => x.Name == name);
                 Console.WriteLine($"Name: {employee.Name}, Salary: {employee.Salary}, Tenant: {employee.TenantId}, CreatedBy: {employee.CreatedById}, CreatedOn: {employee.CreatedOn}, ModifiedBy: {employee.ModifiedById}, ModifiedAt: {employee.ModifiedOn}");
             }
 
@@ -53,14 +64,14 @@ namespace EFInterceptorDemo
             {
                 // SELECT with different tenant returns NULL:
                 Console.WriteLine($"\nselecting employee {name} with tenant id {ctx.TenantId} will return NULL:");
-                var employee = ctx.Employees.FirstOrDefault(x => x.Name == name);
+                employee = ctx.Employees.FirstOrDefault(x => x.Name == name);
                 Console.WriteLine(employee?.ToString());
                 Debug.Assert(employee == null);
             }
 
             using (var ctx = new DataContext(tenantId: 2, userId: 66))
             {
-                var employee = ctx.Employees.First(x => x.Name == name);
+                employee = ctx.Employees.First(x => x.Name == name);
                 ctx.Employees.Remove(employee);
                 Debug.Assert(ctx.SaveChanges() == 1);
             }
